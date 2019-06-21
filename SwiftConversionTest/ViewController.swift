a2893c81c693e43b38af95b7d5e68d9318742a63
Art Huang <arth@oath.com>
2019-06-21 15:45:30 +0800

    Initial Commit

---
+++ SwiftConversionTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwiftConversionTest
+//
+//  Created by Art Huang on 2019/6/21.
+//  Copyright © 2019 Art Huang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
